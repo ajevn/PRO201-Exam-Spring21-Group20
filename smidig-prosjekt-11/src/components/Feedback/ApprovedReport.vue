f8c83c108e5a7f06fd819bb6cc23d12a5ac2baa0
Olav Hartwedt Larsen <ohalar@gmail.com>
2020-12-07 16:25:09 +0100

    Added image from prototype and styling to the h1

--- smidig-prosjekt-11/src/components/Feedback/ApprovedReport.vue
+++ smidig-prosjekt-11/src/components/Feedback/ApprovedReport.vue
@@ -1,6 +1,7 @@
 <template>
-    <div id="approved-report-container" class="text-center align-middle">
-        <h1>Entry Succesful</h1>
+    <div id="approved-report-container" class="text-center">
+        <h1>ENTRY SUCCESFUL</h1>
+        <img src='@/assets/Images/report-sucess-icon.png'>
     </div>
 </template>
 
@@ -14,5 +15,17 @@
 </script>
 
 <style lang="scss" scoped>
-    
+    #approved-report-container {
+
+        h1 {
+            font-size: 50px;
+            font-weight: bold;
+            color: #7EB46B;
+        }
+
+        img {
+            margin: auto;
+            margin-top: 50px;
+        }
+    }
 </style>
