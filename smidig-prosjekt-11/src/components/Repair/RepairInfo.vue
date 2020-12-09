46a364b3585a3a75f4484ecd4bb0a83a5b91ec0d
Andreas Jevnaker <andreasjevn@gmail.com>
2020-12-09 10:30:41 +0100

    separated repair entity into its own component, less code in repairInfo view

--- smidig-prosjekt-11/src/components/Repair/RepairInfo.vue
+++ smidig-prosjekt-11/src/components/Repair/RepairInfo.vue
@@ -15,144 +15,40 @@
                 v-on:click="closeRepair"
             />
         </div>
-        <div>
-            <div id="product-container">
-                <div class="ProductImg">
-                    <img class="product-image-sunbell" src="@/assets/Images/Parts/sunbellProductImage.png" />
-                </div>
-                <span><input type="text" v-model="serialNumber"/></span>
-                <img class="part-imgs" src="@/assets/Images/Parts/battery-removebg-preview.png" alt="parts" />
+        <!-- Grid system for submitted models -->
+        <section id="entity-list-container">
+            <div>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
+                <repair-entity></repair-entity>
 
-                <!-- Div to hold both edit and repair -->
-                <div class="edit-delete-img">
-                    <button @click="editRepair">
-                        <img
-                            class="edit-delete-img-width"
-                            src="@/assets/Images/edit-icon.png"
-                            alt="editbtn"
-                        />
-                    </button>
-
-                    <button>
-                        <img
-                            class="edit-delete-img-width"
-                            src="@/assets/Images/delete-icon.png"
-                            alt="editbtn"
-                        />
-                    </button>
-                </div>
-                <!-- Not needed in this page -->
-                <!--<input type="checkbox" name="approve" />-->
-
-                <hr />
+                <img
+                    v-show="showRepair == false"
+                    id="plus-btn"
+                    class="cursor-pointer hover:bg-gray-400 rounded-full transform hover:translate-y-0.5 hover:translate-x-0.5"
+                    src="@/assets/Images/Icons/plus-icon.png"
+                    v-on:click="addRepair()"
+                />
             </div>
-
-            <div id="product-container">
-                <div class="ProductImg">
-                    <img class="product-image-sunbell" src="@/assets/Images/Parts/sunbellProductImage.png" />
-                </div>
-                <span><input type="text" v-model="serialNumber"/></span>
-                <img class="part-imgs" src="@/assets/Images/Parts/battery-removebg-preview.png" alt="parts" />
-
-                <!-- Div to hold both edit and repair -->
-                <div class="edit-delete-img">
-                    <button @click="editRepair">
-                        <img
-                            class="edit-delete-img-width"
-                            src="@/assets/Images/edit-icon.png"
-                            alt="editbtn"
-                        />
-                    </button>
-
-                    <button>
-                        <img
-                            class="edit-delete-img-width"
-                            src="@/assets/Images/delete-icon.png"
-                            alt="editbtn"
-                        />
-                    </button>
-                </div>
-                <!-- Not needed in this page -->
-                <!--<input type="checkbox" name="approve" />-->
-
-                <hr />
-            </div>
-
-            <div id="product-container">
-                <div class="ProductImg">
-                    <img class="product-image-sunbell" src="@/assets/Images/Parts/sunbellProductImage.png" />
-                </div>
-                <span><input type="text" v-model="serialNumber"/></span>
-                <img class="part-imgs" src="@/assets/Images/Parts/battery-removebg-preview.png" alt="parts" />
-
-                <!-- Div to hold both edit and repair -->
-                <div class="edit-delete-img">
-                    <button @click="editRepair">
-                        <img
-                            class="edit-delete-img-width"
-                            src="@/assets/Images/edit-icon.png"
-                            alt="editbtn"
-                        />
-                    </button>
-
-                    <button>
-                        <img
-                            class="edit-delete-img-width"
-                            src="@/assets/Images/delete-icon.png"
-                            alt="editbtn"
-                        />
-                    </button>
-                </div>
-                <!-- Not needed in this page -->
-                <!--<input type="checkbox" name="approve" />-->
-
-                <hr />
-            </div>
-
-            <div id="product-container">
-                <div class="ProductImg">
-                    <img class="product-image-sunbell" src="@/assets/Images/Parts/sunbellProductImage.png" />
-                </div>
-                <span><input type="text" v-model="serialNumber"/></span>
-                <img class="part-imgs" src="@/assets/Images/Parts/battery-removebg-preview.png" alt="parts" />
-
-                <!-- Div to hold both edit and repair -->
-                <div class="edit-delete-img">
-                    <button @click="editRepair">
-                        <img
-                            class="edit-delete-img-width"
-                            src="@/assets/Images/edit-icon.png"
-                            alt="editbtn"
-                        />
-                    </button>
-
-                    <button>
-                        <img
-                            class="edit-delete-img-width"
-                            src="@/assets/Images/delete-icon.png"
-                            alt="editbtn"
-                        />
-                    </button>
-                </div>
-                <!-- Not needed in this page -->
-                <!--<input type="checkbox" name="approve" />-->
-
-                <hr />
-            </div>
-
-            <img
-                v-show="showRepair == false"
-                id="plus-btn"
-                class="cursor-pointer hover:bg-gray-400 rounded-full transform hover:translate-y-0.5 hover:translate-x-0.5"
-                src="@/assets/Images/Icons/plus-icon.png"
-                v-on:click="addRepair()"
-            />
-        </div>
+        </section>
     </div>
 </template>
 
 <script>
 import PopupSelectRepair from '@/components/UI/PopupSelectRepair.vue';
+import RepairEntity from '@/components/UI/RepairEntity.vue';
 
 export default {
     data() {
@@ -165,7 +61,7 @@ export default {
             showRepair: false
         };
     },
-    components: { PopupSelectRepair },
+    components: { PopupSelectRepair, RepairEntity },
     methods: {
         editRepair() {
             // show entity-spesific repair overlay
@@ -183,52 +79,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#product-container {
-    width: 100%;
-    height: 100px;
-    align-items: center;
-    text-align: center;
-    display: grid;
-    grid-template-columns: 1fr 1fr 40% 1fr 20px;
-    grid-template-rows: auto 2px;
-
-    .product-image-sunbell {
-        width: 55px;
-        margin: auto;
-    }
-
-    input {
-        border: 1.5px solid #423048;
-        border-radius: 5px;
-        background-color: #fffefd;
-        color: #050505;
-        text-align: center;
-        font-weight: bold;
-    }
-
-    .part-imgs {
-        width: 55px;
-        margin: auto;
-    }
-
-    .edit-delete-img {
-        grid-column: 4;
-
-        .edit-delete-img-width {
-            width: 20px;
-            margin-left: 35px;
-            margin-right: 15px;
-        }
-    }
-
-    hr {
-        grid-column: 1/-1;
-        grid-row: 2;
-        width: 93%;
-        margin: auto;
-        border-top: 0.5px solid #707070;
-    }
-}
 #plus-btn {
     margin: auto;
     margin-top: 3vh;
@@ -247,5 +97,10 @@ export default {
     position: relative;
     left: 63.5vw;
     bottom: 34vh;
+}
+#entity-list-container {
+    height: 80vh;
+    width: 100%;
+    overflow-y: scroll;
 }
 </style>
