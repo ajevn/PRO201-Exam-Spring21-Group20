3637f691474ad8a98ea69ece0fc037d8fa29643a
Your Name <you@example.com>
2020-12-10 12:46:49 +0100

    fjernet noe unødvendig

--- smidig-prosjekt-11/src/components/UI/PopupSelectRepair.vue
+++ smidig-prosjekt-11/src/components/UI/PopupSelectRepair.vue
@@ -53,50 +53,43 @@ export default {
                     partNumber: '1',
                     partName: 'Solar Panel',
                     imgName: 'solarPanelCompleteWithCable-removebg-preview',
-                    isChecked: false,
-                    serialNumber: ""
+                    isChecked: false
                 },
                 {
                     partNumber: '2',
                     partName: 'Battery',
                     imgName: 'battery-removebg-preview',
-                    isChecked: false,
-                    serialNumber: ""
+                    isChecked: false
                 },
                 {
                     partNumber: '3',
                     partName: 'Seal',
                     imgName: 'powerSwitchCoverNew-removebg-preview',
-                    isChecked: false,
-                    serialNumber: ""
+                    isChecked: false
                 },
                 {
                     partNumber: '4',
                     partName: 'USB Connector',
                     imgName: 'directUsbPort-removebg-preview',
-                    isChecked: false,
-                    serialNumber: ""
+                    isChecked: false
                 },
                 {
                     partNumber: '5',
                     partName: 'Neck plus light',
                     imgName: 'batteryPackLightUnitComplete-removebg-preview',
-                    isChecked: false,
-                    serialNumber: ""
+                    isChecked: false
                 },
                 {
                     partNumber: '6',
                     partName: 'Torx-5',
                     imgName: 'batteryBoxTorx5-removebg-preview',
-                    isChecked: false,
-                    serialNumber: ""
+                    isChecked: false
                 },
                 {
                     partNumber: '7',
                     partName: 'Torx-6',
                     imgName: 'batteryBoxTorx6-removebg-preview',
-                    isChecked: false,
-                    serialNumber: ""
+                    isChecked: false
                 }
             ],
             partsChosen: []
