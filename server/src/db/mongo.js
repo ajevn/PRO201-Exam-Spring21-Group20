dd4c3a1d0323a52954f61941a5db4efb2537d6df
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-05-26 12:42:59 +0200

    added something

--- server/src/db/mongo.js
+++ server/src/db/mongo.js
@@ -1,5 +1,5 @@
 const monk = require("monk");
-const db = monk(process.env.MONGO_URI);
+const db = monk(process.env.MONGO_URI || process.env.MONGO_ATLAS_URI);
 db.then(() => {
   console.log("connection success");
 }).catch((e) => {
