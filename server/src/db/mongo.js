1442b61e51112066ec071a0f79a94c10dc0e7c61
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-05-31 09:39:56 +0200

    lint

--- server/src/db/mongo.js
+++ server/src/db/mongo.js
@@ -5,13 +5,12 @@ const db = monk(process.env.MONGO_URL);
 db.then(async () => {
   console.log("connection success");
   await addAdmin();
-}).catch((e) => {
+}).catch(() => {
   console.error("Error with monk");
 });
 
 const addAdmin = async () => {
   const user = db.get("users");
-  console.log("test");
   if (await user.find({})) {
     const password = await bcrypt.hash("admin", 10);
     user.insert({
